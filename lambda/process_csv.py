6b1246010b9a6d632ba3ec045020aa34cb822bb8
Farbod Ahmadian <farbodahmadian2014@gmail.com>
2022-01-07 18:07:59 +0330

    Added query for inserting processed Listings CSV to DB

--- lambda/process_csv.py
+++ lambda/process_csv.py
@@ -3,19 +3,31 @@ import boto3
 import csv
 import json
 from os import environ
+import pymysql
 
 s3 = boto3.resource('s3')
+
+# Get DB credentails from SecretsManager
 secrets_manager = boto3.client('secretsmanager')
-
-sm_arn = environ['DB_SECRET_MANAGER_ARN']
-print("Secret Manager: ", sm_arn)
 secret_value = secrets_manager.get_secret_value(
-    SecretId=sm_arn
+    SecretId=environ['DB_SECRET_MANAGER_ARN']
 )
-print(secret_value)
-
 database_secrets = json.loads(secret_value['SecretString'])
-print(database_secrets)
+
+
+# Connect to DB
+try:
+    conn = pymysql.connect(
+        host=database_secrets['host'],
+        user=database_secrets['username'],
+        passwd=database_secrets['password'],
+        db=database_secrets['dbname'],
+    )
+except pymysql.MySQLError as e:
+    print('ERROR: Unexpected error: Could not connect to MySQL instance.')
+    print(e)
+    raise
+
 
 def write_listings_csv_to_db(bucket_name: str, object_key: str) -> None:
     """ Write Listings CSV uploaded file to DB """
@@ -24,9 +36,22 @@ def write_listings_csv_to_db(bucket_name: str, object_key: str) -> None:
 
     lines = csv.reader(data)
     headers = next(lines)
-    for line in lines:
-       print(line)
 
+    item_count = 0
+    with conn.cursor() as cur:
+        for line in lines:
+            query = f"INSERT INTO listings (`id`, `name`, `neighbourhood`, `latitude`, `longitude`, `room_type`, `price`)\
+                      VALUES ('{line[0]}', '{line[1]}', '{line[5]}', '{line[6]}', '{line[7]}', '{line[8]}', '{line[9]}')"
+            try:
+                cur.execute(query)
+            except Exception as e:
+                print(line, query)
+                print(e)
+            else:
+                item_count += 1
+
+    conn.commit()
+    print(f'Item Count: {item_count}')
 
 def handler(event: dict, context):
     for record in event['Records']:                  
