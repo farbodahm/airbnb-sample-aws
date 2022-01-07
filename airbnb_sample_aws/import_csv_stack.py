74117a14bbc2c7536156e44670a28581d3ba1dd3
Farbod Ahmadian <farbodahmadian2014@gmail.com>
2022-01-07 12:12:54 +0330

    Added PyMySql layer

--- airbnb_sample_aws/import_csv_stack.py
+++ airbnb_sample_aws/import_csv_stack.py
@@ -1,10 +1,16 @@
 from aws_cdk import (
+    RemovalPolicy,
     aws_lambda as lambda_,
     aws_s3 as s3,
     aws_s3_notifications as s3_notify,
+    aws_ec2 as ec2,
+    aws_rds as rds,
     Stack,
 )
 from constructs import Construct
+
+
+DATABASE_NAME = 'airbnb'
 
 class ImportCsvStack(Stack):
     """ Stack for creating infrastructure of migrating Airbnb
@@ -14,14 +20,32 @@ class ImportCsvStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        # RDS needs to be setup in a VPC
+        vpc = ec2.Vpc(self, 'migrate-csv-vpc', max_azs=1)
+
         # Create bucket for storing CSVs
-        csv_bucket = s3.Bucket(self, 'CSV-Bucket')
+        csv_bucket = s3.Bucket(self,
+                               'CSV-Bucket',
+                               removal_policy=RemovalPolicy.DESTROY)
+
+        rds_instance = rds.DatabaseInstance(self,
+                                            'DBInstance',
+                                            engine=rds.DatabaseInstanceEngine.mysql(
+                                                version=rds.MysqlEngineVersion.VER_8_0_26),
+                                            instance_type=ec2.InstanceType.of(
+                                                ec2.InstanceClass.BURSTABLE2, ec2.InstanceSize.MICRO
+                                            ),
+                                            vpc=vpc,
+                                            removal_policy=RemovalPolicy.DESTROY,
+                                            deletion_protection=False,
+                                            database_name=DATABASE_NAME)
 
         # Create a lambda function for processing uploaded CSVs
         process_func = lambda_.Function(self, 'ProcessCSV',
                        runtime=lambda_.Runtime.PYTHON_3_9,
                        handler='process_csv.handler',
                        code=lambda_.Code.from_asset('./lambda'),
+                       vpc=vpc,
                        environment={'BUCKET_NAME': csv_bucket.bucket_name})
 
         # Create trigger for Lambda function using suffix
