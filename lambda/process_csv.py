1b0ca42cf3f6179563d25d3d02b176b08c0afcfb
Farbod Ahmadian <farbodahmadian2014@gmail.com>
2022-01-07 12:18:03 +0330

    Added logic of getting DB credentials from Secrets Manager

--- lambda/process_csv.py
+++ lambda/process_csv.py
@@ -1,8 +1,21 @@
 """ Migrate Airbnb CSVs to DB uploaded to S3. """
 import boto3
 import csv
+import json
+from os import environ
 
 s3 = boto3.resource('s3')
+secrets_manager = boto3.client('secretsmanager')
+
+sm_arn = environ['DB_SECRET_MANAGER_ARN']
+print("Secret Manager: ", sm_arn)
+secret_value = secrets_manager.get_secret_value(
+    SecretId=sm_arn
+)
+print(secret_value)
+
+database_secrets = json.loads(secret_value['SecretString'])
+print(database_secrets)
 
 def write_listings_csv_to_db(bucket_name: str, object_key: str) -> None:
     """ Write Listings CSV uploaded file to DB """
@@ -12,7 +25,7 @@ def write_listings_csv_to_db(bucket_name: str, object_key: str) -> None:
     lines = csv.reader(data)
     headers = next(lines)
     for line in lines:
-        print(line)
+       print(line)
 
 
 def handler(event: dict, context):
