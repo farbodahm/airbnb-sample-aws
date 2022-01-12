dc84c52e620f804458c9a3fef51e7f013b2f353a
Farbod Ahmadian <farbodahmadian2014@gmail.com>
2022-01-12 18:34:04 +0330

    Added environment to stack

--- app.py
+++ app.py
@@ -5,8 +5,11 @@ import aws_cdk as cdk
 from airbnb_sample_aws.import_csv_stack import ImportCsvStack
 
 
+ENV_EU = cdk.Environment(account='YOUR-ACCOUNT-ID', region='YOUR-REGION')
+
+
 app = cdk.App()
-stack1 = ImportCsvStack(app, 'ImportCsvStack')
+stack1 = ImportCsvStack(app, 'ImportCsvStack', env=ENV_EU)
 
 cdk.Tags.of(stack1).add('owner', 'farbod')
 
