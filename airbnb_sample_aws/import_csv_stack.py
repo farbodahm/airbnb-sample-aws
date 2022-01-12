b99552ca9d5ec6d0c310184730176ebd1fb3044d
Farbod Ahmadian <farbodahmadian2014@gmail.com>
2022-01-12 18:36:31 +0330

    Seprated a Stack into different Constructs

--- airbnb_sample_aws/import_csv_stack.py
+++ airbnb_sample_aws/import_csv_stack.py
@@ -1,20 +1,14 @@
-from aws_cdk import (
-    RemovalPolicy,
-    aws_lambda as lambda_,
-    aws_s3 as s3,
-    aws_s3_notifications as s3_notify,
-    aws_ec2 as ec2,
-    aws_rds as rds,
-    custom_resources as cr,
-    CustomResource,
-    aws_logs as logs,
-    Stack,
-    Duration,
-)
 from constructs import Construct
+from aws_cdk import (
+    Stack,
+)
 
 
-DATABASE_NAME = 'airbnb'
+from airbnb_sample_aws.import_csv_constructs import (
+    db_initialize,
+    db,
+    process_csv,
+)
 
 
 class ImportCsvStack(Stack):
@@ -25,78 +19,24 @@ class ImportCsvStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # RDS needs to be setup in a VPC
-        vpc = ec2.Vpc(self, 'migrate-csv-vpc', max_azs=2)
+        self.db_construct = db.DatabaseService(
+            self, 
+            'db-construct',
+            'airbnb',
+        )
 
-        # Create bucket for storing CSVs
-        csv_bucket = s3.Bucket(self,
-                               'CSV-Bucket',
-                               removal_policy=RemovalPolicy.DESTROY)
+        db_initialize_construct = db_initialize.DbInitializerService(
+            self,
+            'db-initialize-construct',
+            self.db_construct.vpc,
+            self.db_construct.rds_instance,
+            [self.db_construct.pymysql_lambda_layer,],
+        )
 
-        # Main database
-        rds_instance = rds.DatabaseInstance(self,
-                                            'DBInstance',
-                                            engine=rds.DatabaseInstanceEngine.mysql(
-                                                version=rds.MysqlEngineVersion.VER_8_0_26),
-                                            instance_type=ec2.InstanceType.of(
-                                                ec2.InstanceClass.BURSTABLE2, ec2.InstanceSize.MICRO
-                                            ),
-                                            vpc=vpc,
-                                            removal_policy=RemovalPolicy.DESTROY,
-                                            deletion_protection=False,
-                                            database_name=DATABASE_NAME)
-
-        # PyMySql Lambda layer
-        pymysql_lambda_layer = lambda_.LayerVersion(
-               self, 'MymysqlLambdaLayer',
-               code=lambda_.Code.from_asset('./layers/pymysql.zip'),
-               compatible_runtimes=[lambda_.Runtime.PYTHON_3_9],
-               description='PyMySql Library',
-            )
-
-        # Create a lambda function for initializing database on creation
-        db_initialize_func = lambda_.Function(self, 'InitializeDB',
-                       runtime=lambda_.Runtime.PYTHON_3_9,
-                       handler='initialize_db.handler',
-                       code=lambda_.Code.from_asset('./lambda'),
-                       vpc=vpc,
-                       layers=[pymysql_lambda_layer,],
-                       environment={'DB_SECRET_MANAGER_ARN': rds_instance.secret.secret_arn})
-
-        # Grant DB connection access to Lambda function
-        rds_instance.connections.allow_from(db_initialize_func, ec2.Port.tcp(3306))
-        rds_instance.secret.grant_read(db_initialize_func)
-        rds_instance.grant_connect(db_initialize_func)
-
-        # Create Custom Resource to update database schema
-        my_provider = cr.Provider(self,
-                                  'DbInitializeProvider',
-                                  on_event_handler=db_initialize_func,
-                                  log_retention=logs.RetentionDays.ONE_DAY,
-                                  vpc=vpc,)
-        CustomResource(self, 'Resource1', service_token=my_provider.service_token)
-
-        # Create a lambda function for processing uploaded CSVs
-        process_func = lambda_.Function(self, 'ProcessCSV',
-                       runtime=lambda_.Runtime.PYTHON_3_9,
-                       handler='process_csv.handler',
-                       code=lambda_.Code.from_asset('./lambda'),
-                       vpc=vpc,
-                       layers=[pymysql_lambda_layer,],
-                       memory_size=512,
-                       timeout=Duration.seconds(360),
-                       environment={'DB_SECRET_MANAGER_ARN': rds_instance.secret.secret_arn})
-
-        # Create trigger for Lambda function using suffix
-        notification = s3_notify.LambdaDestination(process_func)
-        notification.bind(self, csv_bucket)
-
-        # Add Create Event only for .csv files
-        csv_bucket.add_object_created_notification(
-           notification, s3.NotificationKeyFilter(suffix='.csv'))
-
-        # Permissions for process_func lambda
-        csv_bucket.grant_read(process_func)
-        rds_instance.connections.allow_from(process_func, ec2.Port.tcp(3306))
-        rds_instance.secret.grant_read(process_func)
-        rds_instance.grant_connect(process_func)
+        process_csv_construct = process_csv.CsvMigrationService(
+            self,
+            'process-csv-construct',
+            self.db_construct.vpc,
+            self.db_construct.rds_instance,
+            [self.db_construct.pymysql_lambda_layer,],
+        )
