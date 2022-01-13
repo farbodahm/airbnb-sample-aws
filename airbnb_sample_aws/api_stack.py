79f883ee28c785ff0d2a8612b1431d8e21952f06
Farbod Ahmadian <farbodahmadian2014@gmail.com>
2022-01-13 14:46:39 +0330

    Add API Stack

---
+++ airbnb_sample_aws/api_stack.py
@@ -0,0 +1,40 @@
+from typing import List
+from constructs import Construct
+from aws_cdk import (
+    Stack,
+    aws_lambda as lambda_,
+    aws_ec2 as ec2,
+    aws_rds as rds,
+)
+
+
+from airbnb_sample_aws.api_constructs import (
+    rooms,
+)
+
+
+class ApiStack(Stack):
+    """ Stack for API infrastructure.
+    """
+
+    def __init__(self, scope: Construct, construct_id: str,
+                 vpc: ec2.IVpc,
+                 rds_instance: rds.DatabaseInstance,
+                 layers: List[lambda_.ILayerVersion],
+                 **kwargs
+                ) -> None:
+        """
+        Parameters:
+        vpc (IVpc): Vpc that the database is in it.
+        rds_instance (DatabaseInstance): RDS database instance to grant permissions.
+        layers (List[ILayerVersion]): Layers that are needed to be associated with lambdas.
+        """
+        super().__init__(scope, construct_id, **kwargs)
+
+        rooms.RoomsApiService(
+            self,
+            'rooms-construct',
+            vpc,
+            rds_instance,
+            layers
+        )
--- app.py
+++ app.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python3
 
+from inspect import stack
 import aws_cdk as cdk
 
 from airbnb_sample_aws.import_csv_stack import ImportCsvStack
-
+from airbnb_sample_aws.api_stack import ApiStack
 
 ENV_EU = cdk.Environment(account='YOUR-ACCOUNT-ID', region='YOUR-REGION')
 
 
 app = cdk.App()
 stack1 = ImportCsvStack(app, 'ImportCsvStack', env=ENV_EU)
+stack2 = ApiStack(
+    app, 
+    'ApiStack',
+    stack1.db_construct.vpc,
+    stack1.db_construct.rds_instance,
+    [stack1.db_construct.pymysql_lambda_layer,],
+    env=ENV_EU
+)
 
 cdk.Tags.of(stack1).add('owner', 'farbod')
+cdk.Tags.of(stack2).add('owner', 'farbod')
+
 
 app.synth()
