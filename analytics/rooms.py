4c45042365000b3d62bc61f29727f19c7794cc53
Farbod Ahmadian <farbodahmadian2014@gmail.com>
2022-02-18 16:49:27 +0330

    Add functionality for creating calendars dataframe

--- analytics/rooms.py
+++ analytics/rooms.py
@@ -4,6 +4,7 @@ This files contains main functionality for analyzing rooms related data.
 from pyspark.sql import (
     SparkSession,
     DataFrame,
+    functions as F,
 )
 
 
@@ -21,15 +22,48 @@ def create_listings_dataframe(spark: SparkSession,
     return listings_df
 
 
+def create_calendars_dataframe(spark: SparkSession,
+                               dataset_path: str) -> DataFrame:
+    ''' Create and return listings data, data frame '''
+    calendars_df = spark.read.csv(
+        header=True,
+        inferSchema=True,
+        path=dataset_path,
+    )
+
+    # Rename price column and remove '$' sign
+    calendars_df = calendars_df.withColumnRenamed(
+        'price', 'calendars_price'
+    ).withColumn(
+        'calendars_price', F.expr("substring(calendars_price, 2, length(calendars_price))")
+    )
+    # Cast calendars_price from str to int
+    calendars_df = calendars_df.withColumn(
+        'calendars_price',
+        calendars_df[
+            'calendars_price'
+        ].cast('int').alias('calendars_price'),
+    )
+
+    return calendars_df
+
+
 def main() -> None:
     '''Starting point of analyzing rooms data '''
     spark = SparkSession.builder.appName(
         'Rooms data'
     ).getOrCreate()
 
+    # TODO: Get path as an argument
     listings_df = create_listings_dataframe(
         spark,
         'S3_PATH_TO_LISTINGS_CSV_DATASET',
+    )
+
+    # TODO: Get path as an argument
+    calendars_df = create_calendars_dataframe(
+        spark,
+        'S3_PATH_TO_CALENDARS_CSV_DATASET',
     )
 
 
