df46887a8155985a939fb15d00b2cb0ae3099314
Farbod Ahmadian <farbodahmadian2014@gmail.com>
2022-02-23 12:57:23 +0330

    Added main driver file

--- analytics/with_scala/src/main/scala/Main.scala
+++ analytics/with_scala/src/main/scala/Main.scala
@@ -1,3 +1,36 @@
-object Main extends App {
-  println("Hello, World!")
+import org.apache.spark.sql.{SaveMode, SparkSession, DataFrame}
+import org.apache.spark.sql.functions.{desc, avg, col, expr}
+
+import Helper.{getPriceAveragePerRoomtype, getTopNRooms}
+import DataFrameBuilder.{createListingsDataFrame, createCalendarsDataFrame}
+import Writer.{writeDataFrameToFile}
+
+/** Driver code */
+object Main {
+
+  // Create Spark session
+  val spark = SparkSession.builder
+    .appName("Rooms Analytics")
+    .master("local[*]")
+    .getOrCreate
+  
+  // Set log level to WARN (Default: INFO)
+  spark.sparkContext.setLogLevel("WARN")
+
+  // TODO: Take the paths as an argument
+  val listingsDf = createListingsDataFrame(spark, "S3_PATH_TO_LISTINGS_DATA")
+  val calendarsDf = createCalendarsDataFrame(spark, "S3_PATH_TO_CALENDARS_DATA")
+
+  // Analytics about room types
+  // TODO: Take the paths as an argument
+  val topNRooms = getTopNRooms(listingsDf, 3)
+  writeDataFrameToFile(topNRooms, "S3_PATH_TO_WRITE_DATA")
+
+  // Analytics about room average price
+  // TODO: Take the paths as an argument
+  for (row <- topNRooms.collect()) {
+    val df = getPriceAveragePerRoomtype(listingsDf, calendarsDf, row.get(0).asInstanceOf[String])
+    writeDataFrameToFile(df, "S3_PATH_TO_WRITE_DATA")
+  }
+
 }
